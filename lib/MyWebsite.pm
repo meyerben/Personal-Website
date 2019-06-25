26c568d717534dba00d3900345fa47a9a347aea6
Ben Meyer <thebenmeyer@gmail.com>
2019-06-25 14:46:57 -0500

    initial commit

---
+++ lib/MyWebsite.pm
@@ -0,0 +1,22 @@
+package MyWebsite;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Load configuration from hash returned by "my_app.conf"
+  my $config = $self->plugin('Config');
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer') if $config->{perldoc};
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+  $r->get('/projects');
+}
+
+1;
