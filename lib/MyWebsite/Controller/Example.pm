aa02c6c44bad8b29ead57451ec911a198978086d
Ben Meyer <thebenmeyer@gmail.com>
2019-07-02 11:29:07 -0500

    removed old example code

--- lib/MyWebsite/Controller/Example.pm
+++ lib/MyWebsite/Controller/Example.pm
@@ -1,12 +1,6 @@
 package MyWebsite::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller';
 
-# This action will render a template
-sub welcome {
-  my $self = shift;
 
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to my site!');
-}
 
 1;
